081edf4c1d84c861a0cf67f9e9c49db0892f7e4b
Netkubb <net.chaosanguan@gmail.com>
2024-11-12 18:08:55 +0700

    change docker

--- deployment.sh
+++ deployment.sh
@@ -3,9 +3,10 @@
 # Build
 for dir in */; do
   if [ -f "$dir/Dockerfile" ]; then
-    image_name=${dir%/}
+    image_name=localhost:5000/${dir%/}
     echo "Building Docker image for $image_name..."
     docker build -t "$image_name" "$dir"
+    docker push "$image_name"
     echo "Built $image_name successfully."
   else
     echo "Skipping $dir"
